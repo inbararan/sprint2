2f74ddc96930a9617ca1185b0b19f8c97a15cbfe
Ophir <ophir.carmel@gmail.com>
2020-12-29 16:53:56 +0200

    blink

--- transmit.py
+++ transmit.py
@@ -1,9 +1,16 @@
 from arduino import set_state
+from color_state import State
 from encoding import encode
 from decoding import decode
 import time
 import os
 import sys
+
+
+def blink():
+    stateAll = State(1, 1, 1, 1, 1)
+    stateNone = State(1, 1, 1, 1, 1)
+
 
 f = open("test.txt", "r")
 states = encode(f)
